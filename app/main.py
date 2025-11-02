22c14f9c59b0eb824ca5d551c4e6f381c6359a6f
nikunjjadav123 <dirensj@gmail.com>
2025-11-02 12:08:29 +0530

    Remove all files to reset repository

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi    import FastAPI,UploadFile, File
-from .kafka_producer import send_to_kafka
-
-app = FastAPI(title="FastAPI + Celery + Redis + Kafka ETL")
-@app.post("/upload")
-async def upload_data(file: UploadFile = File(...)):
-    data = await file.read()
-    send_to_kafka(data.decode('utf-8'))
-    return {"message": "Data sent to Kafka successfully"}
